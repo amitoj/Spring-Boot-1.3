e2a0188db0f4c6ebdb241caba4b47c3fae056eeb
AHanspal <amitojsh@hotmail.com>
2015-08-26 15:01:54 +0100

    first commit

---
+++ demo-jwt-angular-spring/src/main/java/demo/WebApplication.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebApplication {
+ 
+	@Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/*");
+
+        return registrationBean;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
